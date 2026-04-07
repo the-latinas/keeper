9de70d3f9f31060386c7018844ffa560e62b7519
ethanhousley1 <ethanhousley@gmail.com>
2026-04-07 12:19:24 -0600

    Remove girls_struggling import from services __init__

--- ml-pipelines/app/services/__init__.py
+++ ml-pipelines/app/services/__init__.py
@@ -4,10 +4,4 @@ from app.services.retention import (
     predict_retention,
 )
 
-__all__ = ["FEATURE_COLUMNS", "load_retention_pipeline", "predict_retention", "STRUGGLE_FEATURE_COLUMNS", "load_girls_struggling_pipeline", "predict_girls_struggling"]
-
-from app.services.girls_struggling import (
-    STRUGGLE_FEATURE_COLUMNS,
-    load_girls_struggling_pipeline,
-    predict_girls_struggling,
-)
+__all__ = ["FEATURE_COLUMNS", "load_retention_pipeline", "predict_retention"]
